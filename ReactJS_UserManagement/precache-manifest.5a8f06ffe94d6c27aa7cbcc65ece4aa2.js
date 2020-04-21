5476ef28849b2f947eed6513aed8819df914e14a
huynguyen206 <nguyenlehuyuit@gmail.com>
2020-04-21 16:04:19 +0700

    Upload Code Reactjs User Management

---
+++ ReactJS_UserManagement/precache-manifest.5a8f06ffe94d6c27aa7cbcc65ece4aa2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "854f9655457dcdd8218ee12f88f733c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c43083012a3f071b61c7",
+    "url": "/static/css/main.01102b2d.chunk.css"
+  },
+  {
+    "revision": "9a6c007acd2479b369ab",
+    "url": "/static/js/2.1a98a9b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1a98a9b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c43083012a3f071b61c7",
+    "url": "/static/js/main.b69f3f14.chunk.js"
+  },
+  {
+    "revision": "363f612aafcd846dcb76",
+    "url": "/static/js/runtime-main.d002a5f2.js"
+  }
+]);
